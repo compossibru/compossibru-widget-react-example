7c4f817503f0f9446b00b92791c17fef52637fda
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-11-25 10:42:15 +0100

    add apollo client

--- src/app.js
+++ src/app.js
@@ -1,8 +1,45 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import gql from 'graphql-tag';
 
-const App = () => {
-    return <div>Hello React!</div>;
-};
+const client = new ApolloClient({
+    uri: 'https://swapi.apis.guru'
+});
+
+const People = () => (
+    <Query
+        query={gql`
+          {
+              allPeople {
+                people {
+                  id
+                  name
+                }
+              }
+            }
+        `}
+    >
+        {({ loading, error, data }) => {
+            if (loading) return <div>Loading...</div>;
+            if (error) return <div>Error :(</div>;
+
+            return data.allPeople.people.map(({ name, id }) => (
+                <div key={id}>
+                    {name}
+                </div>
+            ));
+        }}
+    </Query>
+);
+
+const App = () => (
+    <ApolloProvider client={client}>
+        <div>
+            <People/>
+        </div>
+    </ApolloProvider>
+);
 
 ReactDOM.render(<App/>, document.getElementById('app'));
